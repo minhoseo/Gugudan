72dfc4b9fd5bf7f3b2aff6b3115c39a73adccac5
jacks <jacks@DESKTOP-AKG8EQ4>
2017-12-14 15:53:11 +0900

    Gugudan v1.0.1

--- src/GugudanInput.java
+++ src/GugudanInput.java
@@ -1,11 +1,1 @@
-import java.util.Scanner;
-
-public class GugudanInput {
-	public static String input(String[] args) {		
-	System.out.println("몇/몇 단을 출력하시겠습니까?: ");
-	Scanner scanner = new Scanner(System.in);
-	String string=scanner.nextLine();
-	
-	return string;
-	}
-}
+//미사용.
--- src/GugudanMain.java
+++ src/GugudanMain.java
@@ -1,12 +1,22 @@
+import java.util.Scanner;
+
 public class GugudanMain {
 	public static void main(String[] args) {
-		String inputValue=GugudanInput.input(args);
-		String[] splitedValue=inputValue.split("/");
-		int first=Integer.parseInt(splitedValue[0]);
-		int second=Integer.parseInt(splitedValue[1]);
-		if(first<2||second<1)
-			System.out.println("2↑/1↑으로 입력해주세요.");
-		else
-			Gugudan.calculate(first,second);
+		while(true) {
+			System.out.println("몇/몇 단을 출력하시겠습니까?: \n(\"end\"를 치시면 종료됩니다.)");
+			Scanner scanner = new Scanner(System.in);
+			String inputValue=scanner.nextLine();
+			if(inputValue.equals("end")) {
+				System.out.println("끝!");
+				break;
+			}
+			String[] splitedValue=inputValue.split("/");
+			int first=Integer.parseInt(splitedValue[0]);
+			int second=Integer.parseInt(splitedValue[1]);
+			if(first<2||second<1)
+				System.out.println("2↑/1↑으로 입력해주세요.");
+			else
+				Gugudan.calculate(first,second);
+		}
 	}
 }
